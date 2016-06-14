9adb123eaa0f98eb993ccb9d6d04b3f9b7dd6b4a
guomindong <guomindongdsl@163.com>
2016-06-14 15:27:42 +0800

    调用天气查询API

--- Tools/js/grid/weather.js
+++ Tools/js/grid/weather.js
@@ -1,39 +1,42 @@
 mui.init();
 
-mui.ready(function() {
+mui.plusReady(function() {
+	var xhr = new plus.net.XMLHttpRequest();
 
-	var url = 'http://apis.baidu.com/apistore/weatherservice/citylist';
+	var url = ' http://apis.baidu.com/thinkpage/weather_api/suggestion';
 
-	var xhr = new XMLHttpRequest();
-	xhr.onload = function() {
-		console.log(this.responseText);
+	function testTap() {
+		console.log("aaa");
 
-	};
-	xhr.onreadystatechange = function() {
-		console.log(this.readyState);
-	};
-	xhr.open('get', 'http://www.baidu.com', true);
-	xhr.send();
+		document.getElementById("weather-submit-id").addEventListener('tap', function() {
+			console.log("bbb");
+		});
+	}
+	testTap();
 
-	mui.ajax(url, {
-		data: {
-			cityname: '%E6%9C%9D%E9%98%B3'
-		},
-		dataType: 'json', //服务器返回json格式数据
-		type: 'get', //HTTP请求类型
-		timeout: 10000, //超时时间设置为10秒；
-		headers: {
-			apikey: 'e5cec900d95b80e69fa2353b2004f90a'
-		},
-		success: function(data) {
-			data = JSON.stringify(data);
-			console.log(data);
-		},
-		error: function(xhr, type, errorThrown) {
-			console.log(xhr);
-			console.log(type);
-			console.log(errorThrown);
-		}
-	});
-
+	//	mui.ajax(url, {
+	//		data: {
+	//			location: '龙州',
+	//			language: 'zh-Hans',
+	//			unit: 'c',
+	//			start: '0',
+	//			days: '1'
+	//		},
+	//		dataType: 'json', //服务器返回json格式数据
+	//		type: 'get', //HTTP请求类型
+	//		timeout: 10000, //超时时间设置为10秒；
+	//		crossDomain: true,
+	//		headers: {
+	//			apikey: 'e5cec900d95b80e69fa2353b2004f90a'
+	//		},
+	//		success: function(data) {
+	//			data = JSON.stringify(data);
+	//			console.log(data);
+	//		},
+	//		error: function(xhr, type, errorThrown) {
+	//			console.log(xhr);
+	//			console.log(type);
+	//			console.log(errorThrown);
+	//		}
+	//	});
 });
